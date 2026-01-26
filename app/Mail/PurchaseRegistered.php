e34a2972f76802c25a09f9da44ef789dba5f8f01
souoda <oda.hpp6@gmail.com>
2026-01-26 12:33:56 +0900

    メール

--- app/Mail/PurchaseRegistered.php
+++ app/Mail/PurchaseRegistered.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Deal;
-use Symfony\Component\Mime\Address;
+use Illuminate\Mail\Mailables\Address;
 
 class PurchaseRegistered extends Mailable
 {
