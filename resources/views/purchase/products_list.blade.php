4737ad45910389ac0f3a62a36f4167f56f712462
souoda <oda.hpp6@gmail.com>
2026-02-17 19:26:20 +0900

    伝票番号

--- resources/views/purchase/products_list.blade.php
+++ resources/views/purchase/products_list.blade.php
@@ -80,7 +80,7 @@
                             <span class="inline-flex items-center px-2 py-1 text-xs font-semibold text-gray-600 bg-gray-100 rounded">未登録</span>
                         @endif
                         <div class="mt-2">
-                            <a href="{{ route('sale.register', ['deal_id' => $item->deal_id, 'buy_item_id' => $item->id]) }}"
+                            <a href="{{ route('sale.register', ['slip_number' => $item->slip_number, 'deal_id' => $item->deal_id, 'buy_item_id' => $item->id]) }}"
                                class="inline-flex items-center px-2 py-1 text-xs font-semibold text-white bg-blue-600 rounded hover:bg-blue-700 transition">
                                 販売登録へ
                             </a>
